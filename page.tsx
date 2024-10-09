6ca402bbc3468098b8e1151dc5d5cfd43aa2019c
Musaratnaz <161160529+Musaratnaz@users.noreply.github.com>
2024-10-10 01:47:38 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import Link from "next/link";
 
-const ContactUs = () => {
+
+  function HomePage  () {
   return (
-    <div>
-      <h1>This is Contact Us Page</h1>
-    </div>
-  )
-}
+      
+      <div>
+          <h1 className="container">HomePage</h1>
+      <h2 className="header">This is HomePage</h2>
 
-export default ContactUs
+       <ul>
+          <li>
+           <Link href="/">Home</Link>
+           </li>
+          <li><Link href="/about" target= 'blank'>About</Link></li>
+           <li><Link href="/contact" target= 'blank'>Contact</Link></li>
+           <li><Link href="/service" target= 'blank'>Service</Link></li>
+        </ul>
+     </div>
+   
+   )
+ }
+    
+  
+
+export default HomePage;             
+        
