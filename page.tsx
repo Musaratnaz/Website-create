7bf7c0da82a2f6415d7120a1a6c04421329eec7d
Musaratnaz <161160529+Musaratnaz@users.noreply.github.com>
2024-10-10 01:37:28 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,24 +1,22 @@
-import Link from "next/link";
-import styles from "../about/about.module.css";
+import Link from 'next/link'
+import styles from "../contact/contact.module.css";
+import React from 'react'
 
-
-
-
-function AboutPage  () {
-   return (
-    <div className={styles.container}>
-   <h2 className={styles.header}>this is About Page</h2>
-
-    
-     <ul className= {styles.list}>
-        <li><a href="/about/about-us">This is About Us Page</a></li>
+function ContactPage () {
+  return (
+    <div className ={styles.container}>
+      <h1 className ={styles.header}>this is Contact Page</h1>
+      
+      
+    <ul className= {styles.list}>
+      <li><a href="/contact/contact-us">This is Contact Us Page</a></li>
          <li><Link href="/">Go to Home Page</Link>
              </li>
-           <li><Link href="/contact" >Go to Contact Page</Link></li>
+           <li><Link href="/about" >Go to about Page</Link></li>
            <li><Link href="/service" >go to Service Page</Link></li>
          </ul>
-       </div>
-    );
+    </div>
+  );
 };
 
-export default AboutPage;
+export default ContactPage 
